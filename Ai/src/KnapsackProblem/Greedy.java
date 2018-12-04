ecf6b1a9da5af231a64c1724232edaea17cd1093
MaxFre <mafr@live.se>
2018-12-04 09:39:29 +0100

    updates

--- Ai/src/KnapsackProblem/Greedy.java
+++ Ai/src/KnapsackProblem/Greedy.java
@@ -7,20 +7,59 @@ public class Greedy {
 	ArrayList<Item> items = new ArrayList<Item>();
 	ArrayList<Knapsack> Knapsacks = new ArrayList<Knapsack>();
 	
-	int dimensions = 3;
+	int value = 3;
 	int weight = 1;
+	
 	
 	
 	public Greedy(){
 		
 		createItems();
 		createKnapsack();
+//		printLayout();
+		greedyAlgortihm();
+	
+	}
+	
+	
+	
+	public void greedyAlgortihm(){
 		
-		System.out.println("Knapsack - Size:" + Knapsacks.get(0).getSize());
+			while(true){
+				int bestValueIndex = 0;
+				double bestValue = 0;
+				
+				if(items.isEmpty() || Knapsacks.get(0).getCurrentWeight() == Knapsacks.get(0).getSize()){
+					break;
+				}
+				
+				for(int i = 0; i<items.size(); i++){
+					int totalWeight = Knapsacks.get(0).getCurrentWeight() + items.get(i).getWeight();
+					if(totalWeight<Knapsacks.get(0).getSize()){
+						if(items.get(i).getRelativeValue()>bestValue){
+						bestValueIndex = i;
+						bestValue = items.get(i).getRelativeValue();						
+						}
+					}
+					
+				}
+				
+				System.out.println("\n");
+				System.out.println("Adding item: " + items.get(bestValueIndex).getWeight());			
+				Knapsacks.get(0).addItem(items.get(bestValueIndex));
+				items.remove(bestValueIndex);
+				System.out.println("current weight: " + Knapsacks.get(0).getCurrentWeight());
+			}
+			System.out.println("Done\n");
+			printLayout();
+	}	
+	
+	
+	public void printLayout(){
+		System.out.println("Knapsack - Size:" + Knapsacks.get(0).getSize() + " current weight: " + Knapsacks.get(0).getCurrentWeight());
 		for(int i = 0; i<items.size(); i++){
 			System.out.println(items.get(i).getName() + " weight: " + items.get(i).getWeight()  + " value: " + items.get(i).getValue()+ " relativeValue: " + items.get(i).getRelativeValue());
 		}
-	
 	}
 	
 	public void createKnapsack(){
@@ -31,9 +70,9 @@ public class Greedy {
 	public void createItems(){
 		
 		for(int i = 1; i<11; i++){
-			Item item = new Item("Item"+i,dimensions, weight);
+			Item item = new Item("Item"+i,value, weight);
 			items.add(item);
-			dimensions++;
+			value++;
 			weight++;
 		}
 	}
